4dd3583be9312efbaf96d596a2cc8c344ae72070
adrian <adrian.saavedra01@uceva.edu.co>
2025-05-12 19:49:03 -0500

    second

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Leer el párrafo
+        System.out.println("Ingrese el párrafo:");
+        String input = sc.nextLine();
+        String[] words = input.trim().split("\\s+");
+
+        // Calcular longitudes
+        int[] wordLengths = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            wordLengths[i] = words[i].length();
         }
+
+        // Leer el ancho de línea
+        System.out.print("Ingrese el ancho máximo de cada línea (M): ");
+        int M = sc.nextInt();
+
+        // Ejecutar el algoritmo
+        WordWrapper justifier = new WordWrapper(words, wordLengths, M);
+        justifier.computeOptimal();
+        justifier.printSolution();
     }
 }
--- src/WordWrapper.java
+++ src/WordWrapper.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
 public class WordWrapper {
+    private final String[] words;
+    private final int[] lengths;
+    private final int M;
+    private final int n;
+    private final int[] dp;
+    private final int[] parent;
+
+    public WordWrapper(String[] words, int[] lengths, int M) {
+        this.words = words;
+        this.lengths = lengths;
+        this.M = M;
+        this.n = lengths.length;
+        this.dp = new int[n + 1];
+        this.parent = new int[n + 1];
+    }
+
+    public void computeOptimal() {
+        final int INF = Integer.MAX_VALUE;
+        for (int i = 0; i <= n; i++) dp[i] = INF;
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += lengths[j];
+                if (j > i) sum++;
+
+                if (sum > M) break;
+
+                int spaces = M - sum;
+                int cost = (j == n - 1) ? 0 : (int)Math.pow(spaces, 3);
+
+                if (dp[i] > cost + dp[j + 1]) {
+                    dp[i] = cost + dp[j + 1];
+                    parent[i] = j + 1;
+                }
+            }
+        }
+
+        System.out.println("Costo mínimo de impresión: " + dp[0]);
+    }
+
+    public void printSolution() {
+        int i = 0;
+        System.out.println("\nDistribución óptima de líneas:");
+        while (i < n) {
+            for (int j = i; j < parent[i]; j++) {
+                System.out.print(words[j] + " ");
+            }
+            System.out.println();
+            i = parent[i];
+        }
+    }
 }
